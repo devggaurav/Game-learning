a39dd35579756f98888a4a099652cfca67236113
gc <dev.gauravchauhan@gmail.com>
2025-01-21 10:12:50 +0530

    added sound

---
+++ composeApp/src/desktopMain/kotlin/com/kmm/clappygc/domain/AudioPlayer.desktop.kt
@@ -0,0 +1,24 @@
+package com.kmm.clappygc.domain
+
+actual class AudioPlayer {
+    actual fun playGameOverSound() {
+    }
+
+    actual fun playJumpSound() {
+    }
+
+    actual fun playFallingSound() {
+    }
+
+    actual fun stopFallingSound() {
+    }
+
+    actual fun playGameSoundInLoop() {
+    }
+
+    actual fun stopGameSound() {
+    }
+
+    actual fun release() {
+    }
+}
