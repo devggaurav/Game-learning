789a7fb1d9a10aa815b3c02e0100c9c58daf5975
gc <dev.gauravchauhan@gmail.com>
2025-01-22 09:33:47 +0530

    added ios sounds

--- composeApp/src/iosMain/kotlin/com/kmm/clappygc/domain/AudioPlayer.ios.kt
+++ composeApp/src/iosMain/kotlin/com/kmm/clappygc/domain/AudioPlayer.ios.kt
@@ -1,7 +1,28 @@
 package com.kmm.clappygc.domain
 
+import platform.AVFAudio.AVAudioPlayer
+import platform.AVFAudio.AVAudioSession
+import platform.AVFAudio.AVAudioSessionCategoryPlayback
+import platform.AVFAudio.setActive
+import platform.Foundation.NSBundle
+import platform.Foundation.NSURL
+import platform.Foundation.NSURL.Companion.fileURLWithPath
+
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+@OptIn(kotlinx.cinterop.ExperimentalForeignApi::class)
 actual class AudioPlayer {
+
+    private var audioPlayers: MutableMap<String, AVAudioPlayer?> = mutableMapOf()
+
+    private var fallingSoundPlayer: AVAudioPlayer? = null
+
+    init {
+
+        val session = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayback, error = null)
+        session.setActive(true, null)
+    }
+
     actual fun playGameOverSound() {
     }
 
@@ -22,4 +43,23 @@ actual class AudioPlayer {
 
     actual fun release() {
     }
+
+    private fun playSound(soundName: String): AVAudioPlayer? {
+        val url = getSoundURL(soundName)
+        val player = url?.let { AVAudioPlayer(it, null) }
+        player?.prepareToPlay()
+        player?.play()
+
+        audioPlayers[soundName] = player
+        return player
+    }
+
+    private fun getSoundURL(resourceName: String): NSURL? {
+        val bundle = NSBundle.mainBundle()
+        val path = bundle.pathForResource(resourceName, "wav")
+        return path?.let {
+            fileURLWithPath(it)
+        }
+    }
+
 }
