b697d5e49d4c572ed2d0717bd5ae70a9c6c273cd
gc <dev.gauravchauhan@gmail.com>
2025-01-23 17:23:54 +0530

    adding sound for desktop

--- composeApp/src/desktopMain/kotlin/com/kmm/clappygc/domain/AudioPlayer.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/kmm/clappygc/domain/AudioPlayer.desktop.kt
@@ -1,7 +1,18 @@
 package com.kmm.clappygc.domain
 
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+import java.nio.file.Files
+import java.nio.file.Paths
+import javax.sound.sampled.SourceDataLine
+
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class AudioPlayer {
+
+    private val audioCache = mutableMapOf<String, ByteArray>()
+    private val playingLines = mutableMapOf<String, SourceDataLine>()
+
+
     actual fun playGameOverSound() {
     }
 
@@ -21,5 +32,28 @@ actual class AudioPlayer {
     }
 
     actual fun release() {
+
     }
+
+    private  fun stopSound(fileName: String){
+        synchronized(playingLines){
+            playingLines[fileName]?.let { line ->
+                line.stop()
+                line.close()
+                playingLines.remove(fileName)
+            }
+        }
+    }
+
+
+    private fun loadAudioFile(fileName: String): ByteArray {
+        val resourcePath = Paths.get("src/commonMain/composeResources/files/$fileName")
+        if (!Files.exists(resourcePath)) {
+            throw FileNotFoundException("File $fileName not found")
+        }
+
+        return FileInputStream(resourcePath.toFile()).use { it.readBytes() }
+    }
+
+
 }
